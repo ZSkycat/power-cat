22bafd27a4892eaa786f31c09865913195013ce9
ZSkycat <skycatmew@gmail.com>
2018-04-20 00:46:39 +0800

    fix Disable QPCore Service (#4) close #4

--- source/Config/ScheduledTask/Disable QPCore Service.ps1
+++ source/Config/ScheduledTask/Disable QPCore Service.ps1
@@ -1,20 +1,56 @@
-# Debug
-# $DebugPreference = 'Continue'
+function ClearGarbage {
+    taskkill.exe /f /t /im QQProtect.exe
+    Set-Service QPCore -StartupType Manual
+}
+
+[int]$maxCount = 0
+[double]$maxWs = 100
+[datetime]$beginTimeWs = Get-Date
+[datetime]$beginTimeNull = Get-Date
 Start-Process 'C:\Program Files (x86)\Tencent\TIM\Bin\QQScLauncher.exe'
-Start-Sleep 5
+
 while ($true) {
-    $process = Get-Process 'tim' | Where-Object { $_.WorkingSet -lt '64mb' }
-    if ($process -eq $null) {
-        Write-Debug 'Clear'
-        taskkill.exe /f /t /im QQProtect.exe
-        Set-Service QPCore -StartupType Manual
-        break;
+    Write-Host 'Sleep'
+    Start-Sleep 1
+
+    $now = Get-Date
+    $process = Get-Process 'tim'
+
+    if ($process) {
+        $beginTimeNull = $now
+
+        # 登录完成检测: 当 Tim 登录窗口退出时
+        if ($process.Length -gt $maxCount) {
+            $maxCount = $process.Length
+        }
+        elseif ($process.Length -lt $maxCount) {
+            ClearGarbage
+            Write-Host 'Clear (count)'
+            break
+        }
+
+        # 登录完成检测: 当 Tim 内存保持稳定时
+        $currentMax = ($process | Measure-Object -Property WS -Maximum).Maximum / 1mb
+
+        if ([System.Math]::Abs($currentMax - $maxWs) -lt 5) {
+            if (($now - $beginTimeWs).Seconds -gt 5) {
+                ClearGarbage
+                Write-Host 'Clear (ws)'
+                break
+            }
+        }
+        else {
+            $beginTimeWs = $now
+        }
+
+        if ($currentMax -gt $maxWs) { $maxWs = $currentMax }
     }
     else {
-        Write-Debug 'Sleep'
-        Start-Sleep 1
+        # 异常检测: 当 Tim 进程不存在时
+        if (($now - $beginTimeNull).Seconds -gt 5) {
+            ClearGarbage
+            Write-Host 'Clear (null)'
+            break
+        }
     }
-}
-if ($DebugPreference -eq 'Continue') {
-    Read-Host 'Exit'
 }
